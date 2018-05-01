d363287b442a39e460f1abcb71f85fd0c9dbf66b
deneme <omerunsal88@gmail.com>
2018-05-01 19:13:47 +0300

    Sorular için CRUD işlemleri tamamlandı

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -9,6 +9,13 @@ use App\User;
 
 class Question extends Model
 {
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    protected $guarded= [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -22,5 +29,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
