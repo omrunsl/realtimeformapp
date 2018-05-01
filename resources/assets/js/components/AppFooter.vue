aeb19df3cbf54b284e21b97829e2645534a36fb3
deneme <omerunsal88@gmail.com>
2018-05-02 01:14:13 +0300

    Vuetfy Vue Router kuruldu

---
+++ resources/assets/js/components/AppFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-footer class="pa-3">
+        Soru&Cevap
+        <v-spacer></v-spacer>
+        <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        name: "app-footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/Tollbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-toolbar>
+      <!--  <v-toolbar-side-icon></v-toolbar-side-icon> -->
+        <v-toolbar-title>Soru&Cevap</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Anasayfa</v-btn>
+            <v-btn flat>Soru Sor</v-btn>
+            <v-btn flat>Kategoriler</v-btn>
+            <v-btn flat>GIRIS Yap</v-btn>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "tollbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
